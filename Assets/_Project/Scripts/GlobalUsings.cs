c347f55aea1748386628cf9b0d6b4f006892b3b1
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2026-03-17 01:59:00 +0700

    chore: Add GlobalUsings with common defines

---
+++ Assets/_Project/Scripts/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Debug = System.Diagnostics.Debug;
+global using uDebug = UnityEngine.Debug;
+//
+global using Object = System.Object;
+global using uObject = UnityEngine.Object;
+//
+global using Random = System.Random;
+global using uRandom = UnityEngine.Random;
